be9a060e4407fe827caa4b9f13c80abfdb92420b
abderaouf_daiffi <40631692+raoufsdxsqdq@users.noreply.github.com>
2022-06-06 12:26:13 +0100

    Update difinitions.h

--- difinitions.h
+++ difinitions.h
@@ -32,3 +32,37 @@
 
 
 
+#include<Arduino.h>
+#define motor_right_1 3 //////////
+#define motor_right_2 4 ///////////       motors configuration 
+#define motor_left_1 7 ///////////
+#define motor_left_2 8 ///////////// 
+
+#define motor_right_speed 5 
+#define motor_left_speed 6
+
+///////////////////////////////////////////////////////////////////////////////////
+
+#define ultra_sonic_front 11
+#define ultra_sonic_right 10
+#define ultra_sonic_left 12
+
+
+#define Trigo_pin_front  13 /////////////////////   ultrasonic sensor configuration 
+#define Trigo_pin_right  A3
+#define Trigo_pin_left  2
+ //////////////////////////////////////////////////////////////////////
+
+#define IR 9     ///////  IR configuration 
+#define led_red 2
+
+//////////////////////////////////////////////////////////////:
+
+#define maze_size 200
+
+
+//////////////////////////////////////////////////////////
+
+
+
+
