15e367e2a2d76915f9b7ae1d207a1c008a04fb5d
abderaouf_daiffi <40631692+raoufsdxsqdq@users.noreply.github.com>
2022-06-06 12:29:11 +0100

    Update U_Sonic.h

--- U_Sonic.h
+++ U_Sonic.h
@@ -10,7 +10,7 @@ long duration_left;
 int distance_left;
 
   ///////////////////////////////////////////////////////////////////////////////////////////////////////////
-void US_operation(){
+void US_operation(int Trigo_pin_com){
   
   digitalWrite(Trigo_pin_com, LOW); /////////////// function for sending signal for ultra sonic //////////
 
@@ -24,7 +24,7 @@ void US_operation(){
  
   ///////////////////////////////////////////////////////////////////////////////////////////////////////////
 void US_front(){                      //////////////// function for the front ultrasonic  //////////////
-  US_operation();
+  US_operation(Trigo_pin_front);
 
   duration_front = pulseIn(ultra_sonic_front, HIGH);
   distance_front = duration_front * 0.034 / 2;
@@ -34,7 +34,7 @@ void US_front(){                      //////////////// function for the front ul
   
   ///////////////////////////////////////////////////////////////////////////////////////////////////////////
   void US_right(){      /////////////////////////func for the right ultra sonic  //////////////////
-  US_operation();
+  US_operation(Trigo_pin_right);
   duration_right = pulseIn(ultra_sonic_right, HIGH);
   distance_right = duration_right * 0.034 / 2;
   
@@ -42,7 +42,7 @@ void US_front(){                      //////////////// function for the front ul
   Serial.println (distance_right);}
   ///////////////////////////////////////////////////////////////////////////////////////////////////////////
 void US_left(){                    ////////////////////// func for the left  ultra sonic /////////////////////
-  US_operation();
+  US_operation(Trigo_pin_left);
   duration_left = pulseIn(ultra_sonic_left, HIGH);
   distance_left = duration_left * 0.034 / 2;
  
@@ -52,3 +52,4 @@ void US_left(){                    ////////////////////// func for the left  ult
   ///////////////////////////////////////////////////////////////////////////////////////////////////////////
 
   
+
