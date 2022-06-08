063b74080ba5a95ff9f3b43b077c07be1fc3c906
abderaouf_daiffi <40631692+raoufsdxsqdq@users.noreply.github.com>
2022-06-08 10:36:35 +0100

    Update M_operations.h

--- M_operations.h
+++ M_operations.h
@@ -1,151 +1,195 @@
+#ifndef M_OPERATION_H
+#define M_OPERATION_H
 #include <Arduino.h>
 #include "difinitions.h"
-#include<Arduino.h>
-#include "difinitions.h"
+#include "U_Sonic.h"
 
-long duration_front;
-int distance_front ; 
-long duration_right;
-int distance_right;
-
-long duration_left;
-int distance_left;
-
-  ///////////////////////////////////////////////////////////////////////////////////////////////////////////
-void US_operation(int Trigo_pin_com){
-  
-  digitalWrite(Trigo_pin_com, LOW); /////////////// function for sending signal for ultra sonic //////////
-
-  delayMicroseconds(2);
-
-  digitalWrite(Trigo_pin_com, HIGH);
-  delayMicroseconds(10);
-  digitalWrite(Trigo_pin_com, LOW);}
-
-
- 
-  ///////////////////////////////////////////////////////////////////////////////////////////////////////////
-void US_front(){                      //////////////// function for the front ultrasonic  //////////////
-  US_operation(Trigo_pin_front);
-
-  duration_front = pulseIn(ultra_sonic_front, HIGH);
-  distance_front = duration_front * 0.034 / 2;
-  
-  Serial.print("Distance_front:  ");
-  Serial.println(distance_front);}
-  
-  ///////////////////////////////////////////////////////////////////////////////////////////////////////////
-  void US_right(){      /////////////////////////func for the right ultra sonic  //////////////////
-  US_operation(Trigo_pin_right);
-  duration_right = pulseIn(ultra_sonic_right, HIGH);
-  distance_right = duration_right * 0.034 / 2;
-  
-  Serial.print("Distance_right:  ");
-  Serial.println (distance_right);}
-  ///////////////////////////////////////////////////////////////////////////////////////////////////////////
-void US_left(){                    ////////////////////// func for the left  ultra sonic /////////////////////
-  US_operation(Trigo_pin_left);
-  duration_left = pulseIn(ultra_sonic_left, HIGH);
-  distance_left = duration_left * 0.034 / 2;
- 
-  Serial.print("Distance_left: ");
-  Serial.println(distance_left);}
-  
-  ///////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-  
-
-
-
+int speed = 0;
+#define kp 2 
+#define h 100
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-void move_forward(int speed_right = 255, int speed_left = 255){ //////////////////////////// the car will move forward /////////////////////////////
+void move_forward(int speed_right = 255, int speed_left = 255)
+{ //////////////////////////// the car will move forward /////////////////////////////
 
-digitalWrite(motor_left_1,HIGH);
-digitalWrite(motor_left_2,LOW);
-digitalWrite(motor_right_2,LOW);
-digitalWrite(motor_right_1,HIGH);
-digitalWrite(motor_right_speed,speed_right);
-digitalWrite(motor_left_speed,speed_left);
-
-
+    digitalWrite(motor_left_1, HIGH);
+    digitalWrite(motor_left_2, LOW);
+    digitalWrite(motor_right_2, LOW);
+    digitalWrite(motor_right_1, HIGH);
+    analogWrite(motor_right_speed, speed_right);
+    analogWrite(motor_left_speed, speed_left);
 }
 
-void move_backward(int speed_right = 255, int speed_left = 255){ //////////////////////////// the car will move forward /////////////////////////////
+void move_backward(int speed_right = 255, int speed_left = 255)
+{ //////////////////////////// the car will move forward /////////////////////////////
 
-digitalWrite(motor_left_1,LOW);
-digitalWrite(motor_left_2,HIGH);
-digitalWrite(motor_right_2,HIGH);
-digitalWrite(motor_right_1,LOW);
-digitalWrite(motor_right_speed,speed_right);
-digitalWrite(motor_left_speed,speed_left);
-
-
+    digitalWrite(motor_left_1, LOW);
+    digitalWrite(motor_left_2, HIGH);
+    digitalWrite(motor_right_2, HIGH);
+    digitalWrite(motor_right_1, LOW);
+    analogWrite(motor_right_speed, speed_right);
+    analogWrite(motor_left_speed, speed_left);
 }
 
-void turn_left(int speed_right = 255, int speed_left = 255){
-
-digitalWrite(motor_left_1,LOW);
-digitalWrite(motor_left_2,LOW);
-digitalWrite(motor_right_2,HIGH);
-digitalWrite(motor_right_1,LOW);
-digitalWrite(motor_right_speed,speed_right); //////>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> speed of turnning
-digitalWrite(motor_left_speed,speed_left); //>>>>>>>>>>>>>>>>>>>>>>>>>>>>> speed variable 
-
-
+void stopp()
+{
+    digitalWrite(motor_left_1, LOW);
+    digitalWrite(motor_left_2, LOW);
+    analogWrite(motor_right_2, LOW); //  stop motors
+    analogWrite(motor_right_1, LOW);
+    // delay(200);
 }
 
-void turn_right(int speed_right = 255, int speed_left = 255){
+void diviate(int speed_right = 255, int speed_left = 255)
+{
+    speed = kp * ((distance_left - distance_right) / 2);
 
-digitalWrite(motor_left_1,HIGH);
-digitalWrite(motor_left_2,LOW);
-digitalWrite(motor_right_2,LOW);
-digitalWrite(motor_right_1,LOW);
-digitalWrite(motor_right_speed,speed_right); //////>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> speed of turnning
-digitalWrite(motor_left_speed,speed_left); //>>>>>>>>>>>>>>>>>>>>>>>>>>>>> speed variable 
+    digitalWrite(motor_left_1, HIGH);
+    digitalWrite(motor_left_2, LOW);
+    digitalWrite(motor_right_2, LOW);
+    digitalWrite(motor_right_1, HIGH);
+    analogWrite(motor_right_speed, speed_right - speed); /// <<< right speed must be greater than the left speed
+    analogWrite(motor_left_speed, speed_left + speed);
+}
 
 
+
+void turn_right()
+{
+
+    steps = 0;
+    while (1)
+    {   
+        stopp();
+        digitalWrite(motor_left_1, HIGH);
+        digitalWrite(motor_left_2, LOW);
+        analogWrite(motor_left_speed, h);
+
+        if (digitalRead(sensor2))
+        {
+
+            Serial.println("hello");
+
+            steps = steps + 1;
+            Serial.println(steps);
+            while (digitalRead(sensor2));
+        }
+
+        if (steps >=16)
+        {
+
+            digitalWrite(motor_right_1, LOW);
+            digitalWrite(motor_right_2, LOW);
+            digitalWrite(motor_left_1, LOW);
+            digitalWrite(motor_left_2, LOW);
+            analogWrite(motor_right_speed, 0);
+            break;
+        }
+    }
+
+    steps = 0;
+    while (1)
+    {
+       move_forward(80, 80);
+      
+        
+        if (digitalRead(sensor2))
+        {
+
+            Serial.println("hello");
+
+            steps = steps + 1;
+            Serial.println(steps);
+            while (digitalRead(sensor2));
+        }
+        if (steps >= 10)
+        {
+
+            
+            
+            digitalWrite(motor_right_1, LOW);
+            digitalWrite(motor_right_2, LOW);
+            digitalWrite(motor_left_1, LOW);
+            digitalWrite(motor_left_2, LOW);
+            analogWrite(motor_right_speed, 0);
+            move_forward(80,80);
+
+            break;
+        }
+    }
 }
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////:
 
-///  go back to the right path
 
+void turn_left()
+{
+    steps = 0;
+    while (1)
+    {   
+        stopp();
+        digitalWrite(motor_right_1, HIGH);
+        digitalWrite(motor_right_2, LOW);
+        analogWrite(motor_right_speed, h);
 
-void  diviate_left (int speed_right = 255, int speed_left = 255){
+        if (digitalRead(sensor))
+        {
 
-digitalWrite(motor_left_1,HIGH);  
-digitalWrite(motor_left_2,LOW);
-digitalWrite(motor_right_2,LOW);
-digitalWrite(motor_right_1,HIGH);
-digitalWrite(motor_right_speed,speed_right); /// <<< right speed must be greater than the left speed 
-digitalWrite(motor_left_speed,speed_left);
+            Serial.println("hello");
 
+            steps = steps + 1;
+            Serial.println(steps);
+            while (digitalRead(sensor));
+        }
 
+        if (steps >=13)
+        {
+
+            digitalWrite(motor_right_1, LOW);
+            digitalWrite(motor_right_2, LOW);
+            digitalWrite(motor_left_1, LOW);
+            digitalWrite(motor_left_2, LOW);
+            analogWrite(motor_right_speed, 0);
+            break;
+        }
+    }
+
+    steps = 0;
+    while (1)
+    {
+       move_forward(80, 80);
+      
+        
+        if (digitalRead(sensor))
+        {
+
+            Serial.println("hello");
+
+            steps = steps + 1;
+            Serial.println(steps);
+            while (digitalRead(sensor));
+        }
+        if (steps >= 10)
+        {
+
+            
+            
+            digitalWrite(motor_right_1, LOW);
+            digitalWrite(motor_right_2, LOW);
+            digitalWrite(motor_left_1, LOW);
+            digitalWrite(motor_left_2, LOW);
+            analogWrite(motor_right_speed, 0);
+            move_forward(80,80);
+
+            break;
+        }
+    }
 }
 
 
 ///  go back to the right path
 
 
-void  diviate_right (int speed_right = 255, int speed_left =255){
 
-digitalWrite(motor_left_1,HIGH);
-digitalWrite(motor_left_2,LOW);
-digitalWrite(motor_right_2,LOW);
-digitalWrite(motor_right_1,HIGH);
-digitalWrite(motor_right_speed,speed_right); //// <<< left speed must be greater than the right speed
-digitalWrite(motor_left_speed,speed_left);
-
-
-}
-
-
-void stop(){
-digitalWrite(motor_left_1,LOW);
-digitalWrite(motor_left_2,LOW);
-digitalWrite(motor_right_2,LOW);  //  stop motors
-digitalWrite(motor_right_1,LOW);
-
-}
+#endif
